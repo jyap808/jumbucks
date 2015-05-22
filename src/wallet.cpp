2f3a4e461ea81f5fbc6e1b7b8e992e33a9dd8245
Julian Yap <julianokyap@gmail.com>
2015-05-21 15:55:42 -1000

    Reduce nStakeCombineThreshold to 500

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 unsigned int nStakeSplitAge = 1 * 24 * 60 * 60;
-int64_t nStakeCombineThreshold = 1000 * COIN;
+int64_t nStakeCombineThreshold = 500 * COIN;
 
 //////////////////////////////////////////////////////////////////////////////
 //
