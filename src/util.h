53544570e6d859965f1a4cf6291d8f18ebb40d68
Julian Yap <julianokyap@gmail.com>
2015-08-13 12:04:18 -1000

    branding

--- src/util.h
+++ src/util.h
@@ -644,7 +644,7 @@ inline uint32_t ByteReverse(uint32_t value)
 //    threadGroup.create_thread(boost::bind(&LoopForever<boost::function<void()> >, "nothing", f, milliseconds));
 template <typename Callable> void LoopForever(const char* name,  Callable func, int64_t msecs)
 {
-    std::string s = strprintf("blackcoin-%s", name);
+    std::string s = strprintf("jumbucks-%s", name);
     RenameThread(s.c_str());
     printf("%s thread start\n", name);
     try
